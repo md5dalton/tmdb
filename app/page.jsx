4c87fce6ff5075544846c93b854306df9960f7ba
md5dalton <md5daltonggs@gmail.com>
2024-01-25 17:20:34 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,22 @@
-export default () => ""
+export default async () => {
+
+    const url = "https://api.themoviedb.org/3/trending/all/day?language=en-US"
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+        }
+    }
+    
+    fetch(url, options)
+      .then(res => res.json())
+      .then(({ results }) => {
+
+        
+
+        // console.log(results.length)
+      })
+      .catch(err => console.error('error:' + err))
+
+}
