8144f58265eb2284b0adae36223720cd95de2867
md5dalton <md5daltonggs@gmail.com>
2024-01-25 17:21:42 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+            "dark-blue": "rgb(3,37,65)",
+            "light-blue": "rgb(1,180,228)",
+            "light-green": "rgb(30,230,169)",
+            "lighter-green": "rgb(192,254,207)",
+            "light-grey": "rgb(227,227,227)",
+            "darker-grey": "rgb(200,200,200)",
+            "vibrant-dark-blue": "rgb(6,37,65)",
+            "vibrant-light-blue": "rgb(0,118,241)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
