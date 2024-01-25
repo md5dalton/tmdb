0f1acd1b12bba7f9ec3479195a7cbb1ea80aa129
md5dalton <md5daltonggs@gmail.com>
2024-01-25 17:20:38 +0200

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,21 @@
+
+
+export default () => (
+    <footer className="
+        line-t
+        w-full
+        grid
+        grid-cols-5
+        fixed
+        bottom-0
+        h-14
+        bg-black
+        font-bold"
+    >
+        {/* <LinkIcon href="/" icon="home2" />
+        <LinkIcon href="/explore" icon="magnifier" />
+        <LinkIcon href="/reel" icon="clapboard-play" />
+        <LinkIcon href="/activity" icon="heart" />
+        <LinkIcon href="/user/current" icon="user" /> */}
+    </footer>
+)
