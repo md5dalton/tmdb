9277863517d71ca782111cf0575d849caf6db5ff
md5dalton <md5daltonggs@gmail.com>
2024-01-28 00:13:14 +0200

    Create Trending.jsx

---
+++ app/(home)/Trending.jsx
@@ -0,0 +1,14 @@
+export default () => (
+    <section className="trending text-black bg-no-repeat">
+        <div className="flex">
+            <h2>Trending</h2>
+            <div>
+                <label>Today</label>
+                <input type="radio" />
+                <label>This week</label>
+                <input type="radio" />
+            </div>
+            <div className="min-h-[350px]"></div>
+        </div>
+    </section>
+)
