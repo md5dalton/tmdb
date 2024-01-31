22bf543bffdcd0a551db5116a662f5441972a3f6
md5dalton <md5daltonggs@gmail.com>
2024-01-31 18:48:32 +0200

    Update Trending.jsx

--- app/(home)/Trending.jsx
+++ app/(home)/Trending.jsx
@@ -1,14 +1,16 @@
 export default () => (
-    <section className="trending text-black bg-no-repeat">
-        <div className="flex">
-            <h2>Trending</h2>
-            <div>
-                <label>Today</label>
-                <input type="radio" />
-                <label>This week</label>
-                <input type="radio" />
+    <section className="trending text-black bg-no-repeat py-4">
+        <div className="flex font-semibold items-center p-4 gap-6">
+            <h2 className="text-2xl">Trending</h2>
+            <div className="flex text-dark-blue border-[1px] border-dark-blue rounded-full overflow-hidden">
+                <label className="h-8 px-5 flex place-items-center">Today</label>
+                <input type="radio" className="hidden" />
+                <label className="h-8 px-5 flex place-items-center bg-dark-blue rounded-full">
+                    <span className="gradient-text">This week</span>
+                </label>
+                <input type="radio" className="hidden" />
             </div>
-            <div className="min-h-[350px]"></div>
         </div>
+        <div className="min-h-[350px]"></div>
     </section>
 )
