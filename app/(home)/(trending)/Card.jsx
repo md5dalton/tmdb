37b2541811667569aaadd5404f530ad110d79260
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-01 18:18:12 +0200

    Update Card.jsx

--- app/(home)/(trending)/Card.jsx
+++ app/(home)/(trending)/Card.jsx
@@ -3,26 +3,28 @@ import {
     EllipsisHorizontalIcon,
 } from "@heroicons/react/24/solid"
 import ProgressBarCircular from "./ProgressBarCircular"
+import Link from "next/link"
 
-export default () => (
+export default ({ name, date, rating, id, type }) => (
     <div className="w-40">
         <div className="w-full h-60 relative flex justify-end p-3">
-            <Image
-                src="/poster.webp"
-                className="h-full w-full bg-black/50 rounded-lg"
-                fill
-            />
+            <Link href={`/${type}/${id}`}>
+                <Image
+                    src="/poster.webp"
+                    className="h-full w-full bg-black/50 rounded-lg"
+                    fill
+                />
+            </Link>
             <button className="z-10 bg-white/50 text-black/50 h-6 w-6 rounded-full flex items-center justify-center">
                 <EllipsisHorizontalIcon className="h-8" />
             </button>
             <div className="absolute h-10 w-10 rounded-full left-3 -bottom-5">
-                <ProgressBarCircular percent={9} />
+                <ProgressBarCircular percent={rating} />
             </div>
-            
         </div>
-        <div>
-            <h3>Movie name</h3>
-            <p>Feb 1, 2024</p>
+        <div className="py-8 px-3">
+            <Link href={`/${type}/${id}`} className="font-bold hover:text-dark-blue">{name}</Link>
+            <p className="opacity-60">{date}</p>
         </div>
     </div>
 )
