f188d2b9bbbd265dd91913c07ac9a221005f9770
md5dalton <md5daltonggs@gmail.com>
2024-02-02 12:11:08 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'image.tmdb.org',
+            // port: '',
+            // pathname: '/my-bucket/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'media.themoviedb.org',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
