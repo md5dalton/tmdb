066e9e3a0f8718a13870a08f4c9b0f463cf2e675
md5dalton <md5daltonggs@gmail.com>
2024-02-02 12:10:47 +0200

    Update Trending.jsx

--- app/(home)/Trending.jsx
+++ app/(home)/Trending.jsx
@@ -1,36 +1,29 @@
+import { getAll } from "@/actions/trending"
 import UList from "@/components/UList"
 import Header from "./(shared)/Header"
 import Section from "./(shared)/Section"
 import Card from "./(trending)/Card"
 
-const trending = [
-    {
-        name: "Aquaman and the Lost Kingdom",
-        date: "Jan 15, 2015",
-        rating: 75
-    },
-    {
-        name: "The Beekeeper",
-        date: "Jan 15, 2015",
-        rating: 15
-    },
-    {
-        name: "Shaun the Sheep Movie",
-        date: "Jan 15, 2015",
-        rating: 55
-    },
-]
-export default () => (
-    <Section className="trending">
-        <Header title="trending" />
-        <div className="min-h-[350px] mt-4">
-            <div className="px-4 w-full overflow-x-auto">
-                <UList
-                    className="flex gap-6 pr-10"
-                    items={trending}
-                    itemHandler={item => <Card {...item} />}
-                />
+export default async () => {
+
+    const req = await getAll()
+    
+    return (
+        <Section className="trending">
+            <Header title="trending" />
+            <div className="min-h-[350px] mt-4">
+                <div className="px-4 w-full overflow-x-auto">
+                    {
+                        req.results ? 
+                        <UList
+                            className="flex gap-6 pr-10"
+                            items={req.results}
+                            itemHandler={item => <Card {...item} />}
+                        /> :
+                        <p>error</p>
+                    }
+                </div>
             </div>
-        </div>
-    </Section>
-)
+        </Section>
+    )
+}
