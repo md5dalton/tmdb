1bfedaceb173ed0e4ccb3f82130fa005dbf27bdc
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-05 19:40:43 +0200

    Update page.jsx

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -1,9 +1,22 @@
+import { getImage } from "@/lib/images"
+import Image from "next/image"
+// import { ColorExtractor } from "react-color-extractor"
+import Info from "./Info"
 
 export default () => {
     
     return (
         <section>
-            {/* <Image */}
+            <div className="h-20 w-20 relative">
+                {/* <ColorExtractor getColors={colors => console.log(colors)}>
+                    <Image
+                        src={getImage("/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg")}
+                        className="h-full w-full bg-black/50 rounded-lg"
+                        fill
+                    />
+                </ColorExtractor> */}
+                <Info />
+            </div>
         </section>
     )
 }
