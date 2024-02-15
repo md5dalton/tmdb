dea625ec670a4ffdbd3b6f525ae24af8ccbc8949
md5dalton <md5daltonggs@gmail.com>
2024-02-15 20:05:31 +0200

    Update Info.jsx

--- app/movie/[id]/Info.jsx
+++ app/movie/[id]/Info.jsx
@@ -1,15 +1,32 @@
+import { formatDate } from "@/lib/date"
 import Description from "./(info)/Description"
 import Header from "./(info)/Header"
 import Images from "./(info)/Images"
 import Misc from "./(info)/Misc"
 import People from "./(info)/People"
 
-export default () => (
+export default ({ 
+    backdrop_path, 
+    poster_path, 
+    title, name, 
+    release_date, first_air_date, 
+    vote_average ,
+    overview,
+    genres,
+    runtime
+}) => (
     <section className="bg-vibrant-dark-blue">
-        <Images />
-        <Header />
-        <Misc />
-        <Description />
+        <Images backdrop={backdrop_path} poster={poster_path} />
+        <Header
+            name={title || name} 
+            year={formatDate(release_date || first_air_date, "yyyy")}
+            vote={vote_average}
+        />
+        <Misc
+            genres={genres}
+            length={runtime}
+        />
+        <Description overview={overview} />
         <People />
     </section>
 )
