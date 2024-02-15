016e8bae6c5308e92f7c9752448a4efc5b66f673
md5dalton <md5daltonggs@gmail.com>
2024-02-15 20:05:47 +0200

    Update Description.jsx

--- app/movie/[id]/(info)/Description.jsx
+++ app/movie/[id]/(info)/Description.jsx
@@ -1,8 +1,6 @@
-export default () => (
+export default ({ overview }) => (
     <div className="px-4">
         <h1 className="text-xl font-semibold">Overview</h1>
-        <p className="py-2">
-            After an amazing first date, Bea and Ben’s fiery attraction turns ice cold — until they find themselves unexpectedly reunited at a destination wedding in Australia. So they do what any two mature adults would do: pretend to be a couple.
-        </p>
+        <p className="py-2">{overview}</p>
     </div>
 )
