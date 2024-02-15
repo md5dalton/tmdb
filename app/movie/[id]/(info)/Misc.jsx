f11bef97a0316106d4f7716db6f42a8d6c1d76dd
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-15 10:21:18 +0200

    Create Misc.jsx

---
+++ app/movie/[id]/(info)/Misc.jsx
@@ -0,0 +1,13 @@
+export default () => (
+    <div className="text-center my-6 bg-black/10 py-2 border-y-[1px] border-y-black/10">
+        <div className="flex justify-center items-center gap-2">
+            <div className="flex gap-2">
+                <p className="uppercase border-darker-grey/50 border-[1px] rounded-md px-1 text-darker-grey/50">nc16</p>
+                <p>02/13/2014 (AU)</p>
+            </div>
+            <div className="h-1 w-1 bg-white rounded-full"></div>
+            <div>1h24m</div>
+        </div>
+        <div className="capitalize">comedy, romance</div>
+    </div>
+)
