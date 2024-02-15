abc44d1960f922cc20038800062dd0d5e3030277
md5dalton <md5daltonggs@gmail.com>
2024-02-15 20:05:35 +0200

    Update Misc.jsx

--- app/movie/[id]/(info)/Misc.jsx
+++ app/movie/[id]/(info)/Misc.jsx
@@ -1,4 +1,6 @@
-export default () => (
+import { formatMinutes } from "@/lib/date";
+
+export default ({ genres, length }) => (
     <div className="text-center my-6 bg-black/10 py-2 border-y-[1px] border-y-black/10">
         <div className="flex justify-center items-center gap-2">
             <div className="flex gap-2">
@@ -6,8 +8,8 @@ export default () => (
                 <p>02/13/2014 (AU)</p>
             </div>
             <div className="h-1 w-1 bg-white rounded-full"></div>
-            <div>1h24m</div>
+            {length && <div>{formatMinutes(length)}</div>}
         </div>
-        <div className="capitalize">comedy, romance</div>
+        <div className="capitalize">{genres.map(({ name }) => name).join(", ")}</div>
     </div>
 )
