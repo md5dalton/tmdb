efd0678c13104ffb0ec788103234cd1ff560d7a2
md5dalton <md5daltonggs@gmail.com>
2024-02-15 20:05:43 +0200

    Update Header.jsx

--- app/movie/[id]/(info)/Header.jsx
+++ app/movie/[id]/(info)/Header.jsx
@@ -1,12 +1,12 @@
 import ProgressBarCircular from "@/components/ProgressBarCircular"
 import { PlayIcon } from "@heroicons/react/24/solid"
 
-export default () => (
+export default ({ name, year, vote, id,  }) => (
     <div>
-        <h1 className="text-2xl font-semibold text-center py-4">Movie name <span className="text-lg opacity-50">(2024)</span></h1>
+        <h1 className="text-2xl font-semibold text-center py-4">{name} <span className="text-lg opacity-50">({year})</span></h1>
         <div className="flex items-center justify-around px-8">
             <div className="flex items-center gap-4">
-                <ProgressBarCircular percent={Math.trunc(5.1 * 10)} />
+                <ProgressBarCircular percent={Math.trunc(vote * 10)} />
                 <h2 className="font-bold">User Score</h2>
             </div>
             <div className="block border-l-[1px] border-l-darker-grey/30 h-6"></div>
