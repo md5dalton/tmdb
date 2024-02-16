6f13cf6cc9452bd740e0fb20efcd40260d5a74bb
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-16 21:32:13 +0200

    Update page.jsx

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -1,12 +1,14 @@
 import { details } from "@/actions/movie"
 import Info from "./Info"
 import Media from "./Media"
+import { query } from "@/apis/yts"
+import { API_URL } from "@/lib/config.yts"
 
 export default async ({ params: { id }, children }) => {
 
     const movie = await details(id)
-
-    // console.log(movie)
+    const torrent = await fetch(API_URL + movie.imdb_id)
+    console.log(movie.imdb_id)
     
     return (
         <div>
