506425b1cad15910de30656d3d66f25e343d704a
md5dalton <md5daltonggs@gmail.com>
2024-02-16 12:55:11 +0200

    Update Modal.jsx

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -2,7 +2,7 @@
 
 export default ({ children, className, toggler }) => <>
     <div className="fixed h-full w-full top-0 left-0 z-50" onClick={toggler}></div>
-    <div className={`fixed w-full bottom-0 left-0 p-4 z-10${className ? " " + className : ""}`}>
+    <div className={`fixed w-full z-50${className ? " " + className : ""}`}>
         {children}
     </div>
 </>
