d7e2ba00ada969d39774609bcefb47a1dfeb6e86
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-19 12:01:54 +0200

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,11 +3,13 @@ import MenuButton from "./MenuButton"
 import SearchButton from "./SearchButton"
 
 export default () => (
-    <header className="sticky top-0 z-50 container bg-dark-blue h-16 flex items-center gap-4 px-4">
-        <MenuButton />
-        <div className="logo font-bold text-2xl tracking-widest grow text-center"> 
-            <Link href="/">CINEDB</Link>
+    <header className="sticky top-0 z-50 bg-dark-blue">
+        <div className="h-16 flex items-center gap-4 px-4 max-w-[1300px] md:px-8 xl:m-auto">
+            <MenuButton className="md:hidden"/>
+            <div className="logo font-bold text-2xl tracking-widest grow text-center md:text-left md:text-3xl"> 
+                <Link href="/">CINEDB</Link>
+            </div>
+            <SearchButton />
         </div>
-        <SearchButton />
     </header>
 )
